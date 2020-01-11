d4267fb41801fce3717f09547864d2679b7106d0
Shrish Mohapatra <shrish.mohapatra@gmail.com>
2020-01-11 15:00:03 -0500

    added models to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Consultant, Client
 
-# Register your models here.
+@admin.register(Consultant, Client)
+class ViewAdmin(admin.ModelAdmin):
+    pass
